70b884dba1d8333f5cb9ae1e12a2ddcdb2d1a619
sitsolutions <sitsolutions@Pakke-Registro.(none)>
2018-07-17 21:13:30 +0000

    server

--- app/common/database.py
+++ app/common/database.py
@@ -7,13 +7,13 @@ __author__ = 'richogtz'
 
 class Database(object):
     # URI = "mongodb://richogtz:cloudstrifeFF7!@127.0.0.1:27017"
-    URI = os.environ.get('MONGOLAB_URI') or "mongodb:/127.0.0.1:27017"
+    URI = os.environ.get('MONGOLAB_URI') or "mongodb://127.0.0.1:27017"
     DATABASE = None
 
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client.get_database()
+        Database.DATABASE = client['pakkeregistro']
 
     @staticmethod
     def insert(collection, data):
