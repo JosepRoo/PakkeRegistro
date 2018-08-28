864e38b837d3e4ed4abcf7618230f86bed26e0d1
Josep Romagosa <josep_roo@live.com>
2018-08-28 13:53:26 -0500

    push

--- app/quote/src/app/app.module.ts
+++ app/quote/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { QuoteService } from './quote.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
