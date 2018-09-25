461b90750416fdbdc226c2b9f468488b31d9873d
Josep Romagosa <jromagosa@sitsolutions.org>
2018-09-25 18:16:23 -0500

    front --build

---
+++ app/weight/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { WeightService } from './weight.service';
+import { Component, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'Pakke-Scales';
+  sub: Subscription;
+  source = interval(1000);
+  weight: any = 0;
+  price: any = 0;
+  pakkePrice: any = 0;
+  percent: any = 0;
+
+  constructor(
+    private weightService: WeightService
+  ) {}
+
+  ngOnInit() {
+    this.sub = this.source.subscribe(_res => {
+      this.weightService.getData().subscribe(res => {
+        this.weight = res.weight;
+        res.result.array.forEach(courrier => {
+          if (courrier.price.ESTAFETA_TERRESTRE_CONSUMO) {
+            this.pakkePrice = courrier.price.ESTAFETA_TERRESTRE_CONSUMO.price;
+          } else if (courrier.price.AEROFLASH_TERRESTRE) {
+            this.price = courrier.price.AEROFLASH_TERRESTRE;
+          }
+        });
+        this.percent = ((this.pakkePrice - this.price) * 100) / this.pakkePrice;
+      });
+    });
+  }
+
+}
---
+++ app/weight/src/app/weight.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeightService {
+
+  headers: HttpHeaders = new HttpHeaders({
+    'Content-Type': 'application/json'
+  });
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getData(): Observable<any> {
+    const data = {
+      origin_zipcode: '11200',
+      destiny_zipcode: '77500'
+    };
+    return this.http.post('/courrier/weight', data, {headers: this.headers}).pipe(map(res => {
+      return res;
+    }));
+  }
+}
+// package/print
