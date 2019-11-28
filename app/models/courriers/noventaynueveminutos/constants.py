c32e603ec5877e9195defa6c99ad2a88a14150c2
Rodrigo Raya <rodrigoraya@pakkemx.local>
2019-11-28 17:45:43 -0600

    99Min url was updated

--- app/models/courriers/noventaynueveminutos/constants.py
+++ app/models/courriers/noventaynueveminutos/constants.py
@@ -1,7 +1,8 @@
 import os
 
 TYPES = {"bicycle99", "bicycle", "mini", "bike99", "bike"}
-URL = "https://deploy-dot-precise-line-76299minutos.appspot.com/2/delivery/shipping_rates"
+# URL = "https://deploy-dot-precise-line-76299minutos.appspot.com/2/delivery/shipping_rates"
+URL = "https://backend-dot-precise-line-76299minutos.appspot.com/2/delivery/shipping_rates"
 API_KEY = os.environ.get("99MINUTOS_KEY")
 USER_ID = int(os.environ.get("99MINUTOS_USER_ID"))
 MAX_WEIGHT = 68
